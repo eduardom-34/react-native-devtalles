be0e010f372db6a80705e15826a1ce8d88cd4c27
CesarLinux <eduardomelchor34@gmail.com>
2026-02-04 19:08:37 -0600

    Estilos y botones

--- 03-counter-app/App.tsx
+++ 03-counter-app/App.tsx
@@ -1,11 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Pressable, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
+
+  const [count, setCount] = useState(10)
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.textHuge}>{count}</Text>
+
+      <Pressable
+      style={styles.floatingButton}
+      onPress={() => setCount(count + 1)}
+      onLongPress={() => setCount(0)}
+      >
+        <Text style={{color: 'white', fontSize: 20}}>+1</Text>
+      </Pressable>
+
+      <StatusBar style="dark" />
     </View>
   );
 }
@@ -17,4 +30,23 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  textHuge: {
+    fontSize: 120,
+    fontWeight: 100
+  },
+
+  floatingButton: {
+    position: 'absolute',
+    bottom: 20,
+    right: 20,
+    backgroundColor: '#65558F',
+    padding: 20,
+    borderRadius: 15,
+    shadowColor: '#000',
+    shadowOffset: {width: 0, height: 4 },
+    shadowOpacity: 0.3,
+    elevation: 3,
+    shadowRadius: 4,
+  }
 });
