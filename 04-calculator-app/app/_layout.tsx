536d7fd5301557ed44481bf8214e42befe16e607
CesarLinux <eduardomelchor34@gmail.com>
2026-02-05 09:17:19 -0600

    Fuentes personalizadas y diseño inicial

--- 04-calculator-app/app/_layout.tsx
+++ 04-calculator-app/app/_layout.tsx
@@ -1,14 +1,23 @@
-import { Slot } from 'expo-router'
-import React from 'react'
-import { Text, View } from 'react-native'
+import { Colors } from '@/constants/theme';
+import { Slot } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { View } from 'react-native';
 
 const RootLayout = () => {
+
+  // const [] = useFonts({
+  //   SpaceMono: require('../asse')
+  // })
+
+
   return (
-    <View>
-      <Text>Header</Text>
+    <View style={{ backgroundColor: Colors.background, flex: 1 }}>
 
       <Slot />
-      <Text>Footeer</Text>
+
+      <StatusBar style='light' />
+
     </View>
   )
 }
--- 04-calculator-app/app/index.tsx
+++ 04-calculator-app/app/index.tsx
@@ -4,7 +4,9 @@ import { Text, View } from 'react-native'
 const index = () => {
     return (
         <View>
-            <Text>CalculatorApp</Text>
+            <Text
+                style={{ fontSize: 40, fontFamily: 'SansSerif', color: 'white' }}
+            >CalculatorApp</Text>
         </View>
     )
 }
