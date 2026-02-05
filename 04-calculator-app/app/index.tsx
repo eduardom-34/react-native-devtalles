1981118de47ae15dc5bea12ad72d5ef023dd5d67
CesarLinux <eduardomelchor34@gmail.com>
2026-02-05 11:31:15 -0600

    Lógica de la calculadora - useCalculator

--- 04-calculator-app/app/index.tsx
+++ 04-calculator-app/app/index.tsx
@@ -1,17 +1,25 @@
 import CalculatorButton from '@/components/CalculatorButton'
 import ThemeText from '@/components/ThemeText'
 import { Colors } from '@/constants/theme'
+import { useCalculator } from '@/hooks/useCalculator'
 import { globalStyles } from '@/styles/global-styles'
 import React from 'react'
 import { View } from 'react-native'
 
-const index = () => {
+const CalculatorApp = () => {
+
+
+  const {
+    formula,
+    buildNumber,
+  } = useCalculator()
+
   return (
     <View style={globalStyles.calculatorContainer}>
 
       {/* Resultados */}
       <View style={{ paddingHorizontal: 30, marginBottom: 20 }}>
-        <ThemeText variant='h1'>50 x 50</ThemeText>
+        <ThemeText variant='h1'>{formula}</ThemeText>
         <ThemeText variant='h2'>250</ThemeText>
       </View>
 
@@ -46,11 +54,11 @@ const index = () => {
 
       <View style={globalStyles.row}>
         {/* Boton1 */}
-        <CalculatorButton label='7' onPress={() => console.log("7")}
+        <CalculatorButton label='7' onPress={() => buildNumber("7")}
         />
-        <CalculatorButton label='8' onPress={() => console.log("8")}
+        <CalculatorButton label='8' onPress={() => buildNumber("8")}
         />
-        <CalculatorButton label='9' onPress={() => console.log("9")}
+        <CalculatorButton label='9' onPress={() => buildNumber("9")}
         />
         <CalculatorButton
           label='x'
@@ -61,11 +69,11 @@ const index = () => {
 
       <View style={globalStyles.row}>
         {/* Boton1 */}
-        <CalculatorButton label='4' onPress={() => console.log("4")}
+        <CalculatorButton label='4' onPress={() => buildNumber("4")}
         />
-        <CalculatorButton label='5' onPress={() => console.log("5")}
+        <CalculatorButton label='5' onPress={() => buildNumber("5")}
         />
-        <CalculatorButton label='6' onPress={() => console.log("6")}
+        <CalculatorButton label='6' onPress={() => buildNumber("6")}
         />
         <CalculatorButton
           label='-'
@@ -77,11 +85,11 @@ const index = () => {
 
       <View style={globalStyles.row}>
         {/* Boton1 */}
-        <CalculatorButton label='1' onPress={() => console.log("1")}
+        <CalculatorButton label='1' onPress={() => buildNumber("1")}
         />
-        <CalculatorButton label='2' onPress={() => console.log("2")}
+        <CalculatorButton label='2' onPress={() => buildNumber("2")}
         />
-        <CalculatorButton label='3' onPress={() => console.log("3")}
+        <CalculatorButton label='3' onPress={() => buildNumber("3")}
         />
         <CalculatorButton
           label='+'
@@ -93,9 +101,9 @@ const index = () => {
 
       <View style={globalStyles.row}>
         {/* Boton1 */}
-        <CalculatorButton label='0' doubleSize onPress={() => console.log("0")}
+        <CalculatorButton label='0' doubleSize onPress={() => buildNumber("0")}
         />
-        <CalculatorButton label='.' onPress={() => console.log(".")}
+        <CalculatorButton label='.' onPress={() => buildNumber(".")}
         />
         <CalculatorButton
           label='='
@@ -108,4 +116,4 @@ const index = () => {
   )
 }
 
-export default index
+export default CalculatorApp
