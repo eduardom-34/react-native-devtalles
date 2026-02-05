579fc37a2cee916df30bda9ad326516bc65fd832
CesarLinux <eduardomelchor34@gmail.com>
2026-02-05 09:02:21 -0600

    ExpoRouter: index y _layout

---
+++ 04-calculator-app/app/_layout.tsx
@@ -0,0 +1,16 @@
+import { Slot } from 'expo-router'
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const RootLayout = () => {
+  return (
+    <View>
+      <Text>Header</Text>
+
+      <Slot />
+      <Text>Footeer</Text>
+    </View>
+  )
+}
+
+export default RootLayout
