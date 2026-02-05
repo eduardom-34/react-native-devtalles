48d88d5a8d54ce4a6bed437185bdea13bbbad88d
CesarLinux <eduardomelchor34@gmail.com>
2026-02-05 10:26:53 -0600

    Botones de la calculadora

--- 04-calculator-app/app/index.tsx
+++ 04-calculator-app/app/index.tsx
@@ -1,4 +1,6 @@
+import CalculatorButton from '@/components/CalculatorButton'
 import ThemeText from '@/components/ThemeText'
+import { Colors } from '@/constants/theme'
 import { globalStyles } from '@/styles/global-styles'
 import React from 'react'
 import { View } from 'react-native'
@@ -6,10 +8,39 @@ import { View } from 'react-native'
 const index = () => {
   return (
     <View style={globalStyles.calculatorContainer}>
-      <ThemeText
-        variant='h1'
-      >50 x 50</ThemeText>
-      <ThemeText variant='h2'>250</ThemeText>
+
+      {/* Resultados */}
+      <View style={{ paddingHorizontal: 30, marginBottom: 20 }}>
+        <ThemeText variant='h1'>50 x 50</ThemeText>
+        <ThemeText variant='h2'>250</ThemeText>
+      </View>
+
+      {/* Filas de botones */}
+
+      <View style={globalStyles.row}>
+        {/* Boton1 */}
+        <CalculatorButton
+          label='C'
+          blackText
+          color={Colors.lightGray}
+          onPress={() => console.log("C")}
+        />
+        <CalculatorButton
+          label='+/-'
+          blackText
+          color={Colors.lightGray}
+          onPress={() => console.log("+/-")}
+        />
+        <CalculatorButton
+          label='del'
+          blackText
+          color={Colors.lightGray}
+          onPress={() => console.log("del")}
+        />
+        <CalculatorButton label='÷' onPress={() => console.log("÷")}
+        />
+      </View>
+
     </View>
   )
 }
