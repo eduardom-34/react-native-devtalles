a521eaa279ff805ec5c77318f64e346e49eb83c0
CesarLinux <eduardomelchor34@gmail.com>
2026-02-11 11:14:57 -0600

    Colores personalizados

--- 05-navigation-app/app/index.tsx
+++ 05-navigation-app/app/index.tsx
@@ -8,13 +8,17 @@ const App = () => {
     <SafeAreaView>
 
       <View className="mt-6 mx-2.5 ">
-        <Text className='text-3xl'
+
+        <Text className='text-5xl'
           style={{ fontFamily: 'WorkSansBlack' }}
         >Hola mundo</Text>
 
 
-        <Text className='text-3xl font-work-medium'>Hola mundo</Text>
-        <Text className='text-3xl font-work-light '>Hola mundo</Text>
+        <Text className='text-4xl font-work-medium text-primary'>Hola mundo</Text>
+        <Text className='text-3xl font-work-light text-secondary'>Hola mundo</Text>
+        <Text className='text-2xl font-work-light text-secondary-100'>Hola mundo</Text>
+        <Text className='text-xl font-work-light text-secondary-200'>Hola mundo</Text>
+        <Text className='text-xl font-work-light text-tertiary'>Hola mundo</Text>
       </View>
 
     </SafeAreaView>
--- 05-navigation-app/tailwind.config.js
+++ 05-navigation-app/tailwind.config.js
@@ -9,6 +9,18 @@ module.exports = {
   presets: [require("nativewind/preset")],
   theme: {
     extend: {
+
+      colors: {
+        primary: '#49129C',
+        secondary: {
+          DEFAULT: '#B40086',
+          100: '#C51297',
+          200: '#831266',
+        },
+        tertiary: '#EF2967'
+      },
+
+
       fontFamily: {
         'work-black': ['WorkSansBlack', 'sans-serif'],
         'work-light': ['WorkSansLight', 'sans-serif'],
