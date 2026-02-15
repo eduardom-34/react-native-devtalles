0740f1aca1c407990411731fbb99b893aea84221
CesarLinux <eduardomelchor34@gmail.com>
2026-02-15 10:57:51 -0600

    Listado de productos y detalle

--- 05-navigation-app/app/(stack)/products/index.tsx
+++ 05-navigation-app/app/(stack)/products/index.tsx
@@ -1,10 +1,33 @@
-import { Text, View } from 'react-native'
+import { products } from '@/store/product.store'
+import { Link } from 'expo-router'
+import { FlatList, Text, View } from 'react-native'
 
 
 const ProductsScreen = () => {
   return (
-    <View>
-      <Text>ProductsScreen</Text>
+    <View className='flex flex-1 px-4'>
+      <FlatList
+        data={products}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <View className='mt-10'>
+            <Text className='text-2xl font-work-black'>{item.title}</Text>
+            <Text className=''>{item.description}</Text>
+
+            <View className='flex flex-row justify-between mt-2'>
+              <Text className='font-work-black'>{item.price}</Text>
+              <Link href={`/(stack)/products/${item.id}`}
+                className='text-primary'
+              >
+                Ver detalles
+              </Link>
+
+            </View>
+
+          </View>
+        )}
+
+      />
     </View>
   )
 }
