5cfcbbe8dbdcb3d9aaee3468a687da54bd5ba74b
CesarLinux <eduardomelchor34@gmail.com>
2026-02-25 21:59:29 -0600

    Cliente de Axios - Primera petición

--- 06-movies-app/app/_layout.tsx
+++ 06-movies-app/app/_layout.tsx
@@ -1,8 +1,12 @@
 import { Text, View } from 'react-native';
 
+import { nowPlayingAction } from '@/core/actions/movies/now-playing.action';
 import '../global.css';
 
 const RootLayout = () => {
+
+  nowPlayingAction();
+
   return (
     <View>
       <Text className='text-2xl font-bold'>RootLayout</Text>
